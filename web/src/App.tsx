97ca21bfb60598264446541ea7722bdc823348be
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-07-08 21:45:34 -0300

    Add Front End

---
+++ web/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import CreateRoom from "./pages/CreateRoom";
+import Room from "./pages/Room";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<CreateRoom />} />
+          <Route path="/room/:id" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
